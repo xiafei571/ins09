a172242d6c46b96df5aae8abbc2cdb33b56c631e
wenxue1412 <wenxue366@gmail.com>
2020-10-11 21:08:12 +0800

    完成了志愿分配的算法的方法

--- src/main/java/com/edu/service/survey/impl/SurveyServiceImpl.java
+++ src/main/java/com/edu/service/survey/impl/SurveyServiceImpl.java
@@ -1,6 +1,8 @@
 package com.edu.service.survey.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -64,9 +66,167 @@ public class SurveyServiceImpl implements SurveyService {
 		}
 	}
 	
-	public Map<String , String> calculateResult(Integer surveyId) { 
+	public Map<String , List<Integer>> calculateResult(Integer surveyId) { 
 		List<SurveyRecord> resultList = surveyMapper.selcetAllBySurveyId(surveyId);
-		System.out.println(resultList);
-		return null;
+		List<Integer> option1 = new ArrayList<Integer>();
+		List<Integer> option2 = new ArrayList<Integer>();
+		List<Integer> option3 = new ArrayList<Integer>();
+		List<Integer> option4 = new ArrayList<Integer>();
+		List<Integer> option5 = new ArrayList<Integer>();
+		List<Integer> option6 = new ArrayList<Integer>();
+		List<Integer> option7 = new ArrayList<Integer>();
+		List<Integer> option8 = new ArrayList<Integer>();
+		List<Integer> option9 = new ArrayList<Integer>();
+		List<Integer> option10 = new ArrayList<Integer>();
+		Map<String, List<Integer>> resultMap = new HashMap<>();
+		for (SurveyRecord surveyRecord: resultList) {
+			String options = surveyRecord.getOptions();
+			String optionsArray[] = options.split(",");
+			if ("1".equals(optionsArray[0])) {
+				option1.add(surveyRecord.getStuId());
+			} else if ("2".equals(optionsArray[0])) {
+				option2.add(surveyRecord.getStuId());
+			} else if ("3".equals(optionsArray[0])) {
+				option3.add(surveyRecord.getStuId());
+			} else if ("4".equals(optionsArray[0])) {
+				option4.add(surveyRecord.getStuId());
+			} else if ("5".equals(optionsArray[0])) {
+				option5.add(surveyRecord.getStuId());
+			} else if ("6".equals(optionsArray[0])) {
+				option6.add(surveyRecord.getStuId());
+			} else if ("7".equals(optionsArray[0])) {
+				option7.add(surveyRecord.getStuId());
+			} else if ("8".equals(optionsArray[0])) {
+				option8.add(surveyRecord.getStuId());
+			} else if ("9".equals(optionsArray[0])) {
+				option9.add(surveyRecord.getStuId());
+			} else if ("10".equals(optionsArray[0])) {
+				option10.add(surveyRecord.getStuId());
+			}
+		}
+		resultMap.put("1", option1);
+		resultMap.put("2", option2);
+		resultMap.put("3", option3);
+		resultMap.put("4", option4);
+		resultMap.put("5", option5);
+		resultMap.put("6", option6);
+		resultMap.put("7", option7);
+		resultMap.put("8", option8);
+		resultMap.put("9", option9);
+		resultMap.put("10", option10);
+		for (String option : resultMap.keySet()) {
+			if (resultMap.get(option).size() < 4) {
+				aa : for (;;) {
+					for (String option_1 : resultMap.keySet()) {
+						if (resultMap.get(option_1).size() > 6) {
+							for (SurveyRecord surveyRecord : resultList) {
+								String options = surveyRecord.getOptions();
+								String optionsArray[] = options.split(",");
+								if (option_1.equals(optionsArray[0]) && option.equals(optionsArray[1])) {
+									resultMap.get(option_1).remove(resultMap.get(option_1).indexOf(surveyRecord.getStuId()));
+									resultMap.get(option).add(surveyRecord.getStuId());
+									if (resultMap.get(option).size() >= 4) {
+										break aa;
+									}
+								} else {
+									break;
+								}
+							} 
+						}
+					}
+					if (resultMap.get(option).size() >= 4) {
+						break;
+					}
+					for (String option_1 : resultMap.keySet()) {
+						if (resultMap.get(option_1).size() > 6) {
+							for (SurveyRecord surveyRecord : resultList) {
+								String options = surveyRecord.getOptions();
+								String optionsArray[] = options.split(",");
+								if (option_1.equals(optionsArray[0]) && option.equals(optionsArray[2])) {
+									resultMap.get(option_1).remove(resultMap.get(option_1).indexOf(surveyRecord.getStuId()));
+									resultMap.get(option).add(surveyRecord.getStuId());
+									if (resultMap.get(option).size() >= 4) {
+										break aa;
+									}
+								} else {
+									break;
+								}
+							} 
+						}
+					}
+					if (resultMap.get(option).size() >= 4) {
+						break;
+					}
+					for (String option_1 : resultMap.keySet()) {
+						if (resultMap.get(option_1).size() > 6) {
+							Integer stuId = resultMap.get(option_1).get(resultMap.get(option_1).size() - 1);
+							resultMap.get(option_1).remove(resultMap.get(option_1).size() - 1);
+							resultMap.get(option).add(stuId);
+							if (resultMap.get(option).size() >= 4) {
+								break aa;
+							}
+						}
+					}
+					if (resultMap.get(option).size() >= 4) {
+						break;
+					}
+					for (String option_1 : resultMap.keySet()) {
+						if (resultMap.get(option_1).size() > 5) {
+							for (SurveyRecord surveyRecord : resultList) {
+								String options = surveyRecord.getOptions();
+								String optionsArray[] = options.split(",");
+								if (option_1.equals(optionsArray[0]) && option.equals(optionsArray[1])) {
+									resultMap.get(option_1).remove(resultMap.get(option_1).indexOf(surveyRecord.getStuId()));
+									resultMap.get(option).add(surveyRecord.getStuId());
+									if (resultMap.get(option).size() >= 4) {
+										break aa;
+									}
+								} else {
+									break;
+								}
+							} 
+						}
+					}
+					if (resultMap.get(option).size() >= 4) {
+						break;
+					}
+					for (String option_1 : resultMap.keySet()) {
+						if (resultMap.get(option_1).size() > 5) {
+							for (SurveyRecord surveyRecord : resultList) {
+								String options = surveyRecord.getOptions();
+								String optionsArray[] = options.split(",");
+								if (option_1.equals(optionsArray[0]) && option.equals(optionsArray[2])) {
+									resultMap.get(option_1).remove(resultMap.get(option_1).indexOf(surveyRecord.getStuId()));
+									resultMap.get(option).add(surveyRecord.getStuId());
+									if (resultMap.get(option).size() >= 4) {
+										break aa;
+									}
+								} else {
+									break;
+								}
+							} 
+						}
+					}
+					if (resultMap.get(option).size() >= 4) {
+						break;
+					}
+					for (String option_1 : resultMap.keySet()) {
+						if (resultMap.get(option_1).size() > 5) {
+							Integer stuId = resultMap.get(option_1).get(resultMap.get(option_1).size() - 1);
+							resultMap.get(option_1).remove(resultMap.get(option_1).size() - 1);
+							resultMap.get(option).add(stuId);
+							if (resultMap.get(option).size() >= 4) {
+								break aa;
+							}
+						}
+					}
+					if (resultMap.get(option).size() >= 4) {
+						break;
+					}
+				}
+			}
+		}
+		System.out.println(resultMap);
+		return resultMap;
 	}
 }
